ce839671f01fa29c0d65eefacdb4570e63cef66c
Vilson Vieira <vilson@void.cc>
2015-11-30 00:04:11 -0200

    Bump to right version of libccv bundle

--- install-libccv.sh
+++ install-libccv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+version=0.1.1
 set -ex
-wget http://void.cc/libccv-0.1.0.tar.gz
-tar -xvzf libccv-0.1.1.tar.gz -C /usr/local/lib
+wget http://void.cc/libccv-$version.tar.gz
+tar -xvzf libccv-$version.tar.gz -C /usr/local/lib
