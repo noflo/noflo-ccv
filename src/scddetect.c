ffe5e18e46cba6619ae7cf6b6114b9e0c599e812
Vilson Vieira <vilson@void.cc>
2015-12-22 17:08:30 -0200

    Return empty list instead of trying to open file

--- src/scddetect.c
+++ src/scddetect.c
@@ -35,37 +35,8 @@ int main(int argc, char** argv)
 		ccv_array_free(seq);
 		ccv_matrix_free(image);
 	} else {
-		FILE* r = fopen(argv[1], "rt");
-		if (argc == 4)
-			chdir(argv[3]);
-		if(r)
-		{
-			size_t len = 1024;
-			char* file = (char*)malloc(len);
-			ssize_t read;
-			while((read = getline(&file, &len, r)) != -1)
-			{
-				while(read > 1 && isspace(file[read - 1]))
-					read--;
-				file[read] = 0;
-				image = 0;
-				ccv_read(file, &image, CCV_IO_RGB_COLOR | CCV_IO_ANY_FILE);
-				assert(image != 0);
-				ccv_scd_param_t params = ccv_scd_default_params;
-				params.size = ccv_size(24, 24);
-				ccv_array_t* seq = ccv_scd_detect_objects(image, &cascade, 1, params);
-				printf("%s %d\n", file, seq->rnum);
-				for (i = 0; i < seq->rnum; i++)
-				{
-					ccv_comp_t* comp = (ccv_comp_t*)ccv_array_get(seq, i);
-					printf("%d %d %d %d %f\n", comp->rect.x, comp->rect.y, comp->rect.width, comp->rect.height, comp->classification.confidence);
-				}
-				ccv_array_free(seq);
-				ccv_matrix_free(image);
-			}
-			free(file);
-			fclose(r);
-		}
+		// TODO: Should error when failed to load image
+		printf("[]");
 	}
 	ccv_scd_classifier_cascade_free(cascade);
 	ccv_disable_cache();
