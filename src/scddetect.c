e3d5573d2c817bbaabd37b715d204335239ba597
Vilson Vieira <vilson@void.cc>
2015-12-23 17:34:38 -0200

    Remove unused timing

--- src/scddetect.c
+++ src/scddetect.c
@@ -12,29 +12,17 @@ const ccv_scd_param_t ccv_scd_params = {
 	},
 };
 
-static unsigned int get_current_time(void)
-{
-	struct timeval tv;
-	gettimeofday(&tv, NULL);
-	return tv.tv_sec * 1000 + tv.tv_usec / 1000;
-}
-
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 	assert(argc >= 3);
 	int i;
 	ccv_enable_default_cache();
 	ccv_dense_matrix_t* image = 0;
 	ccv_scd_classifier_cascade_t* cascade = ccv_scd_classifier_cascade_read(argv[2]);
-	ccv_read(argv[1], &image, CCV_IO_RGB_COLOR | CCV_IO_ANY_FILE);
-	if (image != 0)
-	{
-		unsigned int elapsed_time = get_current_time();
+	ccv_read(argv[1], &image, CCV_IO_RGB_COLOR | CCV_IO_PNG_FILE);
+	if (image != 0) {
 		ccv_array_t* seq = ccv_scd_detect_objects(image, &cascade, 1, ccv_scd_params);
-		elapsed_time = get_current_time() - elapsed_time;
 		printf("[");
-		for (i = 0; i < seq->rnum; i++)
-		{
+		for (i = 0; i < seq->rnum; i++) {
 			ccv_comp_t* comp = (ccv_comp_t*)ccv_array_get(seq, i);
 			if (i == seq->rnum-1)
 				printf("{\"x\": %d, \"y\": %d, \"width\": %d, \"height\": %d, \"confidence\": %f}", comp->rect.x, comp->rect.y, comp->rect.width, comp->rect.height, comp->classification.confidence);
