618b9d10282043b22d1c5407e614332494e186a0
Vilson Vieira <vilson@void.cc>
2015-12-28 17:56:49 -0200

    Check if image has enough rows/cols

--- src/swtdetect.c
+++ src/swtdetect.c
@@ -7,23 +7,27 @@ int main(int argc, char** argv) {
 	ccv_dense_matrix_t* image = 0;
 	ccv_read(argv[1], &image, CCV_IO_GRAY | CCV_IO_ANY_FILE);
 	if (image != 0) {
-		ccv_array_t* words = ccv_swt_detect_words(image, ccv_swt_default_params);
-		if (words) {
-			printf("[");
-			for (i = 0; i < words->rnum; i++) {
-				ccv_rect_t* rect = (ccv_rect_t*)ccv_array_get(words, i);
-				if (i == words->rnum-1)
-					printf("{\"x\": %d, \"y\": %d, \"width\": %d, \"height\": %d}", rect->x, rect->y, rect->width, rect->height);
-				else
-					printf("{\"x\": %d, \"y\": %d, \"width\": %d, \"height\": %d},", rect->x, rect->y, rect->width, rect->height);
+		if (image->rows >= 3 && image->cols >= 3) {
+			ccv_array_t* words = ccv_swt_detect_words(image, ccv_swt_default_params);
+			if (words) {
+				printf("[");
+				for (i = 0; i < words->rnum; i++) {
+					ccv_rect_t* rect = (ccv_rect_t*)ccv_array_get(words, i);
+					if (i == words->rnum-1)
+						printf("{\"x\": %d, \"y\": %d, \"width\": %d, \"height\": %d}", rect->x, rect->y, rect->width, rect->height);
+					else
+						printf("{\"x\": %d, \"y\": %d, \"width\": %d, \"height\": %d},", rect->x, rect->y, rect->width, rect->height);
+				}
+				printf("]");
+				ccv_array_free(words);
 			}
-			printf("]");
-			ccv_array_free(words);
+			ccv_matrix_free(image);
+		} else {
+			printf("[]");
 		}
-		ccv_matrix_free(image);
 	} else {
 		printf("[]");
 	}
-	ccv_drain_cache();
+	ccv_disable_cache();
 	return 0;
 }
